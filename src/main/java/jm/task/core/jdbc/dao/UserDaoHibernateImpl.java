6313ed2d478864ccbafbeef69376a5965a25f798
Shellder55 <thetwillfeed@gmail.com>
2023-07-15 17:40:27 +0600

    Test 2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
         System.out.println("Проверка на работоспособность:)");
+        System.out.println("Вторая проверка");
     }
 
 
